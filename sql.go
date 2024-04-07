fffbd0e622a40bb012d48f0490927cdcb3837c6c
takauma <song.of.restarting.2015@gmail.com>
2024-04-08 05:57:14 +0900

    日付型の場合のミリ秒指定が必ず3桁になるよう修正

--- sql.go
+++ sql.go
@@ -322,7 +322,7 @@ func convFieldToMap(obj interface{}) map[string]string {
 		// 日時型の場合.
 		val, ok := fieldValue.(time.Time)
 		if ok {
-			fieldMap[fieldName] = val.Format("'2006-01-02 15:04:05.999'")
+			fieldMap[fieldName] = val.Format("'2006-01-02 15:04:05.000'")
 			continue
 		}
 
